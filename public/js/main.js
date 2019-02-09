49a4a275130aa828c3f4b1cea63adcd472a985d3
karim cheurfi <karim.cheurfi@gmail.com>
2019-02-09 11:20:57 +0000

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('screen');
+const ctx = canvas.getContext('2d');
+
+// pink = #D175B7
+// blue = #1C2C54
+// black = #010402
+// turquoise = #4BC3B5
+
+ctx.fillStyle = '#1C2C54';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
