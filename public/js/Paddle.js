acd2cec6bffdf0748fd4b155cc7e746d03fa7f63
karim cheurfi <karim.cheurfi@gmail.com>
2019-02-09 16:56:55 +0000

    add basic paddle control

--- public/js/Paddle.js
+++ public/js/Paddle.js
@@ -6,11 +6,30 @@ class Paddle {
         this.width = opts.width;
         this.height = opts.height;
         this.color = opts.color;
+        this.velocity = 7;
+        this.speed = 5;
     }
 
     draw(ctx) {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    moveDown() {
+        this.y += this.velocity;
+        if ((this.y + this.height) > 480) {
+            this.y = 480 - this.height;
+        }
+        console.log("Moving down")
+
+    }
+
+    moveUp() {
+        this.y -= this.velocity;
+        if (this.y < 0) {
+            this.y = 0;
+        }
+        console.log("Moving up")
     }
 
 }
--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,6 @@ import { COLORS } from './constants.js';
 const canvas = document.getElementById('screen');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = COLORS.BLUE;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 // TODO: dynamically compute x and y positions
 const paddleLeft = new Paddle({
     x: 15,
@@ -23,11 +20,43 @@ const paddleRight = new Paddle({
     color: COLORS.PINK
 });
 
-console.log(paddleRight);
+function clearCanvas() {
+    ctx.fillStyle = COLORS.BLUE;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
 
-paddleLeft.draw(ctx);
-paddleRight.draw(ctx);
-// create a paddle class
-// instantiate two paddles with different colors
-// have them drawn on the canvas
+// Create canvas
+function updateGame() {
+}
+
+function renderGame() {
+    clearCanvas();
+    paddleLeft.draw(ctx);
+    paddleRight.draw(ctx);
+}
+
+function gameLoop() {
+    updateGame();
+    renderGame();
+    requestAnimationFrame(gameLoop);
+}
+
+window.addEventListener('keydown', e => {
+
+    console.log(e.keyCode);
+
+    if(e.keyCode === 40) {
+        paddleLeft.moveDown();
+    } else if(e.keyCode === 38) {
+        paddleLeft.moveUp();
+    } else if (e.keyCode === 87) {
+        paddleRight.moveUp();
+    } else if (e.keyCode === 83) {
+        paddleRight.moveDown();
+    }
+
+});
+
+
+gameLoop(); // later to be CyberPong.start()
 // have them react (move) to user Input
