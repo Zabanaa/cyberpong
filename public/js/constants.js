256a961f4c52a157d4fc6f0dfb4a016204fc0ddb
karim cheurfi <karim.cheurfi@gmail.com>
2019-02-09 11:27:55 +0000

    move colors definition into a constant file

---
+++ public/js/constants.js
@@ -0,0 +1,6 @@
+export const COLORS = {
+    PINK: '#D175B7',
+    BLUE: '#1C2C54',
+    BLACK: '#010402',
+    TURQUOISE: '#4BC3B5'
+};
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,6 @@
+import { COLORS } from './constants.js';
 const canvas = document.getElementById('screen');
 const ctx = canvas.getContext('2d');
 
-// pink = #D175B7
-// blue = #1C2C54
-// black = #010402
-// turquoise = #4BC3B5
-
-ctx.fillStyle = '#1C2C54';
+ctx.fillStyle = COLORS.BLUE;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
